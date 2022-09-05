0812fda8a545476bdcb21f643355fb7a8ce75581
fh32000 <fh32000@gmail.com>
2022-09-05 23:06:14 +0300

    update config

--- config/ckeditor5Classic.php
+++ config/ckeditor5Classic.php
@@ -44,12 +44,22 @@ return [
             'imageUpload',
             '|',
             'insertTable',
+            'tableColumn',
+            'tableRow',
+            'mergeTableCells',
             '|',
             'alignment:left',
             'alignment:center',
             'alignment:right',
             'alignment:justify',
             '|'
+        ],
+        'table' => [
+            'contentToolbar' => [
+                'tableColumn',
+                'tableRow',
+                'mergeTableCells',
+            ]
         ],
         'image' => [
             'toolbar' => [
@@ -68,10 +78,14 @@ return [
             ]
         ],
         'heading' => [
-            'options'=> [
-                [ 'model'=> 'paragraph', 'title'=> 'Paragraph', 'class'=> 'ck-heading_paragraph'] ,
-                [ 'model'=> 'heading2', 'view'=> 'h2', 'title'=> 'Heading 2', 'class'=> 'ck-heading_heading2' ],
-                [ 'model'=> 'heading3', 'view'=> 'h3', 'title'=> 'Heading 3', 'class'=> 'ck-heading_heading3' ]
+            'options' => [
+                ['model' => 'paragraph', 'title' => 'Paragraph', 'class' => 'ck-heading_paragraph'],
+                ['model' => 'heading1', 'view' => 'h1', 'title' => 'Heading 1', 'class' => 'ck-heading_heading1'],
+                ['model' => 'heading2', 'view' => 'h2', 'title' => 'Heading 2', 'class' => 'ck-heading_heading2'],
+                ['model' => 'heading3', 'view' => 'h3', 'title' => 'Heading 3', 'class' => 'ck-heading_heading3'],
+                ['model' => 'heading4', 'view' => 'h4', 'title' => 'Heading 4', 'class' => 'ck-heading_heading4'],
+                ['model' => 'heading5', 'view' => 'h5', 'title' => 'Heading 5', 'class' => 'ck-heading_heading5'],
+                ['model' => 'heading6', 'view' => 'h6', 'title' => 'Heading 6', 'class' => 'ck-heading_heading6'],
             ],
         ],
     ]
